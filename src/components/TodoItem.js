ca20973388dbaa1b4c3142e1c9762a3e7159cd65
Paulina Płachno <paulina1plachno@gmail.comgit config -- global user.namecleargit configgit config --global user.email paulina1plachno@gmail.com>
2022-06-03 12:48:28 +0200

    initial commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const TodoItem = props => {
+    return <div className={`card ${props.element.isCompleted ? 'completed' : ''}`} key={props.element.id}>
+        <h2> {props.element.title}</h2>
+        <button onClick={() => props.markClicked(props.element.id)}>Zakończone </button>
+        </div>
+
+} 
+
+export default TodoItem;
